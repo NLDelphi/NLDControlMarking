9151031b92f9a5bb3446d3e83f9b2ceaa9b8eb2a
marcel <marcel@72e85ab5-d49d-6249-ada7-36e3088ace03>
2008-04-14 16:39:46 +0000

    Added items remotely
    C:\NLDelphi\opensource\ericlang
    C:\NLDelphi\opensource\frank
    C:\NLDelphi\opensource\goleztrol
    C:\NLDelphi\opensource\henkie
    C:\NLDelphi\opensource\jelmer vos
    C:\NLDelphi\opensource\josaikema
    C:\NLDelphi\opensource\marcel
    C:\NLDelphi\opensource\matthijs
    C:\NLDelphi\opensource\mhemmes
    C:\NLDelphi\opensource\ngln
    C:\NLDelphi\opensource\psychomark
    C:\NLDelphi\opensource\rckoenes
    C:\NLDelphi\opensource\richard
    C:\NLDelphi\opensource\svg_1986
    C:\NLDelphi\opensource\walterheck
    C:\NLDelphi\opensource\wok
    C:\NLDelphi\opensource\dany
    C:\NLDelphi\opensource\dees
    C:\NLDelphi\opensource\doggo18
    C:\NLDelphi\opensource\eagle00789
    
    git-svn-id: https://svn.apada.nl/svn/NLDelphi-opensource/ngln/nldmarkablecontrol@11 72e85ab5-d49d-6249-ada7-36e3088ace03

--- NLDControlMarking.pas
+++ NLDControlMarking.pas
@@ -1,270 +1,460 @@
-(*****************************************************************************
-Omschrijving:
-  Een MarkableControl toont m.b.v. een Timer-event een 'wandelend'
-selectiekader rond een T(Win)Control van 2 pixels breed, ongeveer gelijk aan
-het markeringskader van MS Excel. In een andere vorm wordt dit ook wel
-Marching Ants genoemd.
-Zie ook http://www.nldelphi.com/Forum/showthread.php?threadid=16633
-
-Gebruik:
-  Het MarkableControl is er in twee versies, een TNLDMarkableGraphicControl
-en een TNLDMarkableCustomControl. De markeringsstreepjes worden getekend op de
-Canvas van het Control en worden verwijderd met Paint. Deze Paint moet in een
-descender gemaakt worden, aangezien de Paint van TCustom/GraphicControl niets
-doet en ik eventuele descenders niet in de weg wilde lopen.
-  De markering is in en uit te schakelen met de methods Mark en UnMark. Intern
-wordt een counter bijgehouden hoevaak het Control gemarkeerd is. Bij een
-gelijk aantal aanroepen van UnMark wordt de markering opgeheven, of als de
-property Marked op False wordt gezet.
-
-Oorsprong:
-  Ik wilde voor een planning-programma een duidelijk in het oog springend
-kenmerk aan een Planning-item toevoegen om aan te geven dat die gemarkeerd was
-(wat ik bijv. gebruik bij een zoekopdracht). Een border was niet duidelijk
-genoeg vanwege de aanwezige gridlines, en een kleur was niet duidelijk genoeg,
-want ik werkte al met meerdere kleuren voor de Planning-items.
-
-Voordelen:
-* In één applicatie, meerdere markeringen (dit kan bijvoorbeeld niet in Excel)
-
-Nadelen:
-* Indien een descender een Control plaatst, daar waar de markeringsstreepjes
-  moeten gaan lopen, zijn die niet meer zichtbaar; het canvas ligt per
-  definitie altijd op de onderste laag. Verplaatsen van dat Control is dan de
-  enige oplossing.
-* Bij vele markeringen flinke belasting van de processor vanwege het aantal
-  Timers.
-
-Bugs:
-* Voor zover ik weet zijn er geen bugs.
-
-Openstaande ideeën:
-* Property MarkWidth
-* Property MarkSpeed
-* Property MarkColor
-* Property MarkLength
-* ....
-
-Nog te onderzoeken:
-* Het tekenen van de streepjes gebeurd nu nog met de Pixels-property van het
-  Canvas, maar misschien is het efficienter om een custom linetype te
-  definiëren en deze met LineTo/PolyLine te tekenen.
-  Zie ook http://www.nldelphi.com/Forum/showthread.php?t=22368
-
-Veel plezier...
-
-Albert de Weerd (NGLN)
-******************************************************************************)
-
-unit NLDMarkableControl;
+unit NLDControlMarking;
 
 interface
 
 uses
-  Extctrls, Controls, Classes;
+  Classes, Windows, Graphics, ExtCtrls, Controls;
 
 type
-  TNLDMarkableGraphicControl = class(TGraphicControl)
+  TMarkDirection = (mdClockwise, mdCounterclockwise);
+
+  TMarkBounds = (mbClient, mbWindow);
+
+const
+  DefMarkBounds = mbWindow;
+  DefMarkColor = clBlack;
+  DefMarkDirection = mdClockwise;
+  DefMarkInterval = 40;
+  DefMarkLength = 9;
+  DefMarkShift = 1;
+  DefMarkWidth = 2;
+  DefSpaceColor = clDefault;
+
+type
+  TCustomControlMarker = class(TComponent)
   private
-    MarkTimer: TTimer;
-    MarkCounter: Integer;
-    procedure OnMarkTimer(Sender: TObject);
-    function GetMarked: Boolean;
+    FControls: TList;
+    FMarkBounds: TMarkBounds;
+    FMarkBrush: LOGBRUSH;
+    FMarkColor: TColor;
+    FMarkDirection: TMarkDirection;
+    FMarkLength: Byte;
+    FMarkPen: HPEN;
+    FMarkPosition: Word;
+    FMarkShift: Byte;
+    FMarkTimer: TTimer;
+    FMarkWidth: Byte;
+    FPenStyle: array[0..1] of Integer;
+    FSpaceBrush: LOGBRUSH;
+    FSpaceColor: TColor;
+    FSpacePen: HPEN;
+    function GetControlCount: Integer;
+    function GetMarkInterval: Cardinal;
+    procedure MarkTimer(Sender: TObject);
+    procedure PaintControls;
+    procedure ResetPens(const AltSpaceColor: TColor = clDefault);
+    procedure SetMarkColor(const Value: TColor);
+    procedure SetMarkInterval(const Value: Cardinal);
+    procedure SetMarkLength(const Value: Byte);
+    procedure SetMarkWidth(const Value: Byte);
+    procedure SetSpaceColor(const Value: TColor);
   protected
-    procedure SetMarked(const Value: Boolean); virtual;
+    procedure Notification(AComponent: TComponent; Operation: TOperation);
+      override;
+    property MarkBounds: TMarkBounds read FMarkBounds write FMarkBounds
+      default DefMarkBounds;
+    property MarkColor: TColor read FMarkColor write SetMarkColor
+      default DefMarkColor;
+    property MarkDirection: TMarkDirection read FMarkDirection
+      write FMarkDirection default DefMarkDirection;
+    property MarkInterval: Cardinal read GetMarkInterval write SetMarkInterval
+      default DefMarkInterval;
+    property MarkLength: Byte read FMarkLength write SetMarkLength
+      default DefMarkLength;
+    property MarkShift: Byte read FMarkShift write FMarkShift
+      default DefMarkShift;
+    property MarkWidth: Byte read FMarkWidth write SetMarkWidth
+      default DefMarkWidth;
+    property SpaceColor: TColor read FSpaceColor write SetSpaceColor
+      default DefSpaceColor;
   public
     constructor Create(AOwner: TComponent); override;
-    procedure Mark;
-    procedure UnMark;
+    destructor Destroy; override;
+    procedure MarkControl(AControl: TControl);
+    procedure UnmarkControl(AControl: TControl);
+    procedure UnmarkControls;
+    property ControlCount: Integer read GetControlCount;
+  end;
+
+  TNLDControlMarker = class(TCustomControlMarker)
   published
+    property MarkBounds;
+    property MarkColor;
+    property MarkDirection;
+    property MarkInterval;
+    property MarkLength;
+    property MarkShift;
+    property MarkWidth;
+    property SpaceColor;
+  end;
+
+  TMarkableGraphicControl = class(TGraphicControl)
+  private
+    FMarkCounter: Integer;
+    function GetMarked: Boolean;
+    procedure SetMarked(const Value: Boolean);
+  protected
+    function GetMarker: TCustomControlMarker; virtual;
+  public
+    procedure Mark; virtual;
+    procedure UnMark; virtual;
     property Marked: Boolean read GetMarked write SetMarked default False;
   end;
 
-  TNLDMarkableCustomControl = class(TCustomControl)
+  TMarkableCustomControl = class(TCustomControl)
   private
-    MarkTimer: TTimer;
-    MarkCounter: Integer;
-    procedure OnMarkTimer(Sender: TObject);
+    FMarkCounter: Integer;
     function GetMarked: Boolean;
+    procedure SetMarked(const Value: Boolean);
   protected
-    procedure SetMarked(const Value: Boolean); virtual;
+    function GetMarker: TCustomControlMarker; virtual;
   public
-    constructor Create(AOwner: TComponent); override;
-    procedure Mark;
-    procedure UnMark;
-  published
+    procedure Mark; virtual;
+    procedure UnMark; virtual;
     property Marked: Boolean read GetMarked write SetMarked default False;
   end;
+
+function DefControlMarker: TNLDControlMarker;
 
 procedure Register;
 
 implementation
 
-{$R *.RES}
-
-uses
-  Graphics;
-
 procedure Register;
 begin
-  RegisterComponents('NLDelphi', [TNLDMarkableGraphicControl,
-    TNLDMarkableCustomControl]);
+  RegisterComponents('NLDelphi', [TNLDControlMarker]);
 end;
 
-{ TNLDMarkableGraphicControl }
+var
+  FControlMarker: TNLDControlMarker;
 
-constructor TNLDMarkableGraphicControl.Create(AOwner: TComponent);
+function DefControlMarker: TNLDControlMarker;
 begin
-  inherited;
-  MarkTimer := TTimer.Create(Self);
-  MarkTimer.Enabled := False;
-  MarkTimer.Interval := 40;
-  MarkTimer.OnTimer := OnMarkTimer;
-  MarkCounter := 0;
+  if FControlMarker = nil then
+    FControlMarker := TNLDControlMarker.Create(nil);
+  Result := FControlMarker;
 end;
 
-function TNLDMarkableGraphicControl.GetMarked: Boolean;
+type
+  TPublicControl = class(TControl);
+  TPublicGraphicControl = class(TGraphicControl);
+  TPublicCustomControl = class(TCustomControl);
+
+{ TCustomControlMarker }
+
+constructor TCustomControlMarker.Create(AOwner: TComponent);
 begin
-  Result := MarkCounter > 0;
+  inherited Create(AOwner);
+  FControls := TList.Create;
+  FMarkBounds := DefMarkBounds;
+  FMarkColor := DefMarkColor;
+  FMarkDirection := DefMarkDirection;
+  FMarkShift := DefMarkShift;
+  FMarkWidth := DefMarkWidth;
+  FMarkTimer := TTimer.Create(nil);
+  FMarkTimer.Enabled := False;
+  FMarkTimer.Interval := DefMarkInterval;
+  FMarkTimer.OnTimer := MarkTimer;
+  FMarkBrush.lbStyle := BS_SOLID;
+  FSpaceBrush.lbStyle := BS_SOLID;
+  FSpaceColor := DefSpaceColor;
+  SetMarkLength(DefMarkLength);
+  ResetPens;
 end;
 
-procedure TNLDMarkableGraphicControl.Mark;
+destructor TCustomControlMarker.Destroy;
 begin
-  Inc(MarkCounter);
-  MarkTimer.Enabled := MarkCounter > 0;
+  FMarkTimer.Enabled := False;
+  FControls.Free;
+  FMarkTimer.Free;
+  inherited Destroy;
 end;
 
-procedure TNLDMarkableGraphicControl.OnMarkTimer(Sender: TObject);
-const
-  MarkLength = 9; //Lengte en tussenruimte van de markeringsstreepjes
-  MarkShift = 1; //Opschuifwaarde van de markeringsstreepjes
-                 //bij het OnMarkTimerInterval-Event
+function TCustomControlMarker.GetControlCount: Integer;
+begin
+  Result := FControls.Count;
+end;
+
+function TCustomControlMarker.GetMarkInterval: Cardinal;
+begin
+  Result := FMarkTimer.Interval;
+end;
+
+procedure TCustomControlMarker.MarkControl(AControl: TControl);
+begin
+  if AControl is TMarkableGraphicControl then
+    Inc(TMarkableGraphicControl(AControl).FMarkCounter)
+  else if AControl is TMarkableCustomControl then
+    Inc(TMarkableCustomControl(AControl).FMarkCounter);
+  if FControls.IndexOf(AControl) < 0 then
+  begin
+    FControls.Add(AControl);
+    FreeNotification(AControl);
+  end;
+  if FControls.Count = 1 then
+    FMarkTimer.Enabled := True;
+end;
+
+procedure TCustomControlMarker.MarkTimer(Sender: TObject);
+begin
+  PaintControls;
+  Inc(FMarkPosition, FMarkShift);
+  if FMarkPosition >= (2 * FMarkLength) then
+    FMarkPosition := 0;
+end;
+
+procedure TCustomControlMarker.Notification(AComponent: TComponent;
+  Operation: TOperation);
+begin
+  if Operation = opRemove then
+    FControls.Remove(AComponent);
+  inherited Notification(AComponent, Operation);
+end;
+
+procedure TCustomControlMarker.PaintControls;
 var
   i: Integer;
-  PixelColor: TColor;
+  DC: HDC;
+  Rect: TRect;
+  Points: array[0..4] of TPoint;
 begin
-  for i := 0 to Width - 1 do
+  for i := 0 to FControls.Count - 1 do
   begin
-    if Odd((i + MarkTimer.Tag) div MarkLength) then
-      PixelColor := Canvas.Brush.Color
-    else
-      PixelColor := clBlack;
-    Canvas.Pixels[Width - i - 1, 0] := PixelColor;
-    Canvas.Pixels[Width - i, 1] := PixelColor;
-    Canvas.Pixels[i + 1, Height - 1] := PixelColor;
-    Canvas.Pixels[i, Height - 2] := PixelColor;
+    DC := 0;
+    if TControl(FControls[i]) is TGraphicControl then
+      DC := TPublicGraphicControl(FControls[i]).Canvas.Handle
+    else if TControl(FControls[i]) is TCustomControl then
+      DC := TPublicCustomControl(FControls[i]).Canvas.Handle
+    else if TControl(FControls[i]) is TWinControl then
+      if FMarkBounds = mbClient then
+        DC := GetDC(TWinControl(FControls[i]).Handle)
+      else
+        DC := GetWindowDC(TWinControl(FControls[i]).Handle);
+    if DC <> 0 then
+      try
+        if FMarkBounds = mbClient then
+          Rect := TControl(FControls[i]).ClientRect
+        else
+          with TControl(FControls[i]) do
+            Rect := Classes.Rect(0, 0, Width, Height);
+        InflateRect(Rect, -FMarkWidth div 2, -FMarkWidth div 2);
+        if FSpaceColor = clDefault then
+          ResetPens(TPublicControl(FControls[i]).Color);
+        case MarkDirection of
+          mdClockwise:
+            with Rect do
+            begin
+              SelectObject(DC, FSpacePen);
+              Points[0] := Point(Left, -FMarkPosition + FMarkLength);
+              Points[1] := Point(Left, Bottom);
+              Points[2] := BottomRight;
+              Points[3] := Point(Right, Top);
+              Points[4] := Point(FMarkWidth, Top);
+              Polyline(DC, Points[0], 5);
+              SelectObject(DC, FMarkPen);
+              Points[0] := Point(Left, -FMarkPosition);
+              Polyline(DC, Points[0], 5);
+            end;
+          mdCounterclockwise:
+            with Rect do
+            begin
+              SelectObject(DC, FSpacePen);
+              Points[0] := Point(-FMarkPosition + FMarkLength, Top);
+              Points[1] := Point(Right, Top);
+              Points[2] := BottomRight;
+              Points[3] := Point(Left, Bottom);
+              Points[4] := Point(Left, FMarkWidth);
+              Polyline(DC, Points[0], 5);
+              SelectObject(DC, FMarkPen);
+              Points[0] := Point(-FMarkPosition, Top);
+              Polyline(DC, Points[0], 5);
+            end;
+        end;
+      finally
+        if TControl(FControls[i]) is TWinControl then
+          ReleaseDC(TWinControl(FControls[i]).Handle, DC);
+      end;
   end;
-  for i := 0 to Height - 1 do
-  begin
-    if Odd((i + MarkTimer.Tag) div MarkLength) then
-      PixelColor := Canvas.Brush.Color
-    else
-      PixelColor := clBlack;
-    Canvas.Pixels[0, i] := PixelColor;
-    Canvas.Pixels[1, i - 1] := PixelColor;
-    Canvas.Pixels[Width - 1, Height - i - 1] := PixelColor;
-    Canvas.Pixels[Width - 2, Height - i] := PixelColor;
-  end;
-  //MarkTimer.Tag = aantal keren dat de markeringsstreepjes zijn opgeschoven
-  MarkTimer.Tag := MarkTimer.Tag + MarkShift;
 end;
 
-procedure TNLDMarkableGraphicControl.SetMarked(const Value: Boolean);
+procedure TCustomControlMarker.ResetPens(
+  const AltSpaceColor: TColor = clDefault);
 begin
-  if Value <> GetMarked then
+  if AltSpaceColor = clDefault then
+  begin
+    FMarkBrush.lbColor := ColorToRGB(FMarkColor);
+    DeleteObject(FMarkPen);
+    FMarkPen := ExtCreatePen(PS_GEOMETRIC or PS_USERSTYLE, FMarkWidth,
+      FMarkBrush, 2, @FPenStyle);
+    FSpaceBrush.lbColor := ColorToRGB(FSpaceColor);
+    DeleteObject(FSpacePen);
+    FSpacePen := ExtCreatePen(PS_GEOMETRIC or PS_USERSTYLE, FMarkWidth,
+      FSpaceBrush, 2, @FPenStyle);
+  end else if Cardinal(ColorToRGB(AltSpaceColor)) <> FSpaceBrush.lbColor then
+  begin
+    FSpaceBrush.lbColor := ColorToRGB(AltSpaceColor);
+    DeleteObject(FSpacePen);
+    FSpacePen := ExtCreatePen(PS_GEOMETRIC or PS_USERSTYLE, FMarkWidth,
+      FSpaceBrush, 2, @FPenStyle);
+  end;
+end;
+
+procedure TCustomControlMarker.SetMarkColor(const Value: TColor);
+begin
+  if FMarkColor <> Value then
+  begin
+    FMarkColor := Value;
+    ResetPens;
+  end;
+end;
+
+procedure TCustomControlMarker.SetMarkInterval(const Value: Cardinal);
+begin
+  FMarkTimer.Interval := Value;
+end;
+
+procedure TCustomControlMarker.SetMarkLength(const Value: Byte);
+begin
+  if FMarkLength <> Value then
+  begin
+    FMarkLength := Value;
+    FPenStyle[0] := FMarkLength;
+    FPenStyle[1] := FMarkLength;
+    ResetPens;
+  end;
+end;
+
+procedure TCustomControlMarker.SetMarkWidth(const Value: Byte);
+begin
+  if FMarkWidth <> Value then
+  begin
+    FMarkWidth := Value;
+    ResetPens;
+  end;
+end;
+
+procedure TCustomControlMarker.SetSpaceColor(const Value: TColor);
+begin
+  if FSpaceColor <> Value then
+  begin
+    FSpaceColor := Value;
+    ResetPens;
+  end;
+end;
+
+procedure TCustomControlMarker.UnmarkControl(AControl: TControl);
+
+  procedure RemoveControl;
+  begin
+    FControls.Remove(AControl);
+    AControl.Repaint;
+    if AControl is TWinControl then
+      if FMarkBounds = mbWindow then
+        RedrawWindow(TWinControl(AControl).Handle, nil, 0,
+          RDW_FRAME or RDW_INVALIDATE or RDW_NOCHILDREN);
+    RemoveFreeNotification(AControl);
+  end;
+
+begin
+  if FControls.IndexOf(AControl) > -1 then
+  begin
+    if AControl is TMarkableGraphicControl then
+    begin
+      Dec(TMarkableGraphicControl(AControl).FMarkCounter);
+      if TMarkableGraphicControl(AControl).FMarkCounter = 0 then
+        RemoveControl;
+    end else if AControl is TMarkableCustomControl then
+    begin
+      Dec(TMarkableCustomControl(AControl).FMarkCounter);
+      if TMarkableCustomControl(AControl).FMarkCounter = 0 then
+        RemoveControl;
+    end else
+      RemoveControl;
+  end;
+  if FControls.Count = 0 then
+    FMarkTimer.Enabled := False;
+end;
+
+procedure TCustomControlMarker.UnmarkControls;
+var
+  i: Integer;
+begin
+  for i := FControls.Count - 1 downto 0 do
+    UnmarkControl(FControls[i]);
+end;
+
+{ TMarkableGraphicControl }
+
+function TMarkableGraphicControl.GetMarked: Boolean;
+begin
+  Result := FMarkCounter > 0;
+end;
+
+function TMarkableGraphicControl.GetMarker: TCustomControlMarker;
+begin
+  Result := DefControlMarker;
+end;
+
+procedure TMarkableGraphicControl.Mark;
+begin
+  GetMarker.MarkControl(Self);
+end;
+
+procedure TMarkableGraphicControl.SetMarked(const Value: Boolean);
+begin
+  if GetMarked <> Value then
     if Value then
       Mark
     else
     begin
-      MarkCounter := 0;
+      FMarkCounter := 1;
       UnMark;
     end;
 end;
 
-procedure TNLDMarkableGraphicControl.UnMark;
+procedure TMarkableGraphicControl.UnMark;
 begin
-  if MarkCounter > 0 then
-    Dec(MarkCounter);
-  MarkTimer.Enabled := MarkCounter > 0;
-  if MarkCounter = 0 then
-    Paint;
+  GetMarker.UnmarkControl(Self);
 end;
 
 { TMarkableCustomControl }
 
-constructor TNLDMarkableCustomControl.Create(AOwner: TComponent);
+function TMarkableCustomControl.GetMarked: Boolean;
 begin
-  inherited;
-  MarkTimer := TTimer.Create(Self);
-  MarkTimer.Enabled := False;
-  MarkTimer.Interval := 40;
-  MarkTimer.OnTimer := OnMarkTimer;
-  MarkCounter := 0;
+  Result := FMarkCounter > 0;
 end;
 
-function TNLDMarkableCustomControl.GetMarked: Boolean;
+function TMarkableCustomControl.GetMarker: TCustomControlMarker;
 begin
-  Result := MarkCounter > 0;
+  Result := DefControlMarker;
 end;
 
-procedure TNLDMarkableCustomControl.Mark;
+procedure TMarkableCustomControl.Mark;
 begin
-  Inc(MarkCounter);
-  MarkTimer.Enabled := MarkCounter > 0;
+  GetMarker.MarkControl(Self);
 end;
 
-procedure TNLDMarkableCustomControl.OnMarkTimer(Sender: TObject);
-const
-  MarkLength = 9;
-  MarkShift = 1;
-var
-  i: Integer;
-  PixelColor: TColor;
+procedure TMarkableCustomControl.SetMarked(const Value: Boolean);
 begin
-  for i := 0 to Width - 1 do
-  begin
-    if Odd((i + MarkTimer.Tag) div MarkLength) then
-      PixelColor := Canvas.Brush.Color
-    else
-      PixelColor := clBlack;
-    Canvas.Pixels[Width - i - 1, 0] := PixelColor;
-    Canvas.Pixels[Width - i, 1] := PixelColor;
-    Canvas.Pixels[i + 1, Height - 1] := PixelColor;
-    Canvas.Pixels[i, Height - 2] := PixelColor;
-  end;
-  for i := 0 to Height - 1 do
-  begin
-    if Odd((i + MarkTimer.Tag) div MarkLength) then
-      PixelColor := Canvas.Brush.Color
-    else
-      PixelColor := clBlack;
-    Canvas.Pixels[0, i] := PixelColor;
-    Canvas.Pixels[1, i - 1] := PixelColor;
-    Canvas.Pixels[Width - 1, Height - i - 1] := PixelColor;
-    Canvas.Pixels[Width - 2, Height - i] := PixelColor;
-  end;
-  MarkTimer.Tag := MarkTimer.Tag + MarkShift;
-end;
-
-procedure TNLDMarkableCustomControl.SetMarked(const Value: Boolean);
-begin
-  if Value <> GetMarked then
+  if GetMarked <> Value then
     if Value then
       Mark
     else
     begin
-      MarkCounter := 0;
+      FMarkCounter := 1;
       UnMark;
     end;
 end;
 
-procedure TNLDMarkableCustomControl.UnMark;
+procedure TMarkableCustomControl.UnMark;
 begin
-  if MarkCounter > 0 then
-    Dec(MarkCounter);
-  MarkTimer.Enabled := MarkCounter > 0;
-  if MarkCounter = 0 then
-    Paint;
+  GetMarker.UnmarkControl(Self);
 end;
+
+initialization
+
+finalization
+  if FControlMarker <> nil then
+    FControlMarker.Free;
 
 end.
